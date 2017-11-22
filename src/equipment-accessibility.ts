03b4a23fe9eebf5c42a470aa937c8203726628ed
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-22 12:34:30 +0100

    Added missing equipment-accessibility files

---
+++ src/equipment-accessibility.ts
@@ -0,0 +1,60 @@
+import { Length, LengthSchema } from './units'
+import SimpleSchema from 'simpl-schema'
+
+export interface EquipmentAccessibility {
+  height: Length
+  languages: Array<string>
+  isRaised: boolean
+  isBraille: boolean
+  hasSpeech: boolean
+  isHighContrast: boolean
+  hasLargePrint: boolean
+  isVoiceActivated: boolean
+  hasHeadPhoneJack: boolean
+  isEasyToUnderstand: boolean
+}
+
+export const EquipmentAccessibilitySchema = new SimpleSchema({
+  height: {
+    type: LengthSchema
+  },
+  languages: {
+    type: Array,
+    defaultValue: []
+  },
+  'languages.$': {
+    type: String
+  },
+  isRaised: {
+    type: Boolean,
+    defaultValue: false
+  },
+  isBraille: {
+    type: Boolean,
+    defaultValue: false
+  },
+  hasSpeech: {
+    type: Boolean,
+    defaultValue: false
+  },
+  isHighContrast: {
+    type: Boolean,
+    defaultValue: false
+  },
+  hasLargePrint: {
+    type: Boolean,
+    defaultValue: false
+  },
+  isVoiceActivated: {
+    type: Boolean,
+    defaultValue: false
+  },
+  hasHeadPhoneJack: {
+    type: Boolean,
+    defaultValue: false
+  },
+  isEasyToUnderstand: {
+    type: Boolean,
+    defaultValue: false
+  }
+})
---
+++ test/equipment-accessibility.test.ts
@@ -0,0 +1,43 @@
+import { EquipmentAccessibilitySchema } from '../src/equipment-accessibility'
+
+const equipmentFixture = {
+  height: '90 .. 120cm',
+  languages: ['en', 'de'],
+  isRaised: true,
+  isBraille: true,
+  hasSpeech: true,
+  isHighContrast: true,
+  hasLargePrint: true,
+  isVoiceActivated: true,
+  hasHeadPhoneJack: true,
+  isEasyToUnderstand: true
+}
+
+const allValidFixtures = Object.freeze([equipmentFixture])
+
+const invalidEquipmentAccessibilityFixture = {
+  height: [],
+  languages: 'foo'
+}
+
+const allInvalidFixtures = Object.freeze([
+  {},
+  invalidEquipmentAccessibilityFixture
+])
+
+describe('EquipmentAccessibilitySchema Schema', () => {
+  it('tests field as invalid', () => {
+    allInvalidFixtures.forEach(value => {
+      const context = EquipmentAccessibilitySchema.newContext()
+      context.validate(value)
+      expect(context.isValid()).toBeFalsy()
+    })
+  })
+  it('tests field as valid', () => {
+    allValidFixtures.forEach(value => {
+      const context = EquipmentAccessibilitySchema.newContext()
+      context.validate(value)
+      expect(context.isValid()).toBeTruthy()
+    })
+  })
+})
