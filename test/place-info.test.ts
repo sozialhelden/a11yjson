908c7cf17b78e9e2aa03bf85f1b662b66b4d57d6
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-23 11:38:15 +0100

    Included place properties in place info test

--- test/place-info.test.ts
+++ test/place-info.test.ts
@@ -1,11 +1,22 @@
 import { PlaceInfoSchema } from '../src/place-info';
 import { validPointGeometryFixture } from './geometry.test';
+import { placePropertiesMinimumFixture } from './place-properties.test';
 
-const placeFixture = {
+const placeMinimumFixture = {
+  properties: placePropertiesMinimumFixture,
   geometry: validPointGeometryFixture
 };
 
-const allValidFixtures = Object.freeze([placeFixture]);
+const placeWithOptionalsFixture = {
+  formatVersion: '1',
+  properties: placePropertiesMinimumFixture,
+  geometry: validPointGeometryFixture
+};
+
+const allValidFixtures = Object.freeze([
+  placeMinimumFixture,
+  placeWithOptionalsFixture
+]);
 
 const invalidPlaceFixture = {
   bar: []
@@ -22,12 +33,12 @@ describe('PlaceInfoSchema Schema', () => {
       expect(context.isValid()).toBeFalsy();
     });
   });
-  // it('tests field as valid', () => {
-  //   allValidFixtures.forEach(value => {
-  //     const context = PlaceInfoSchema.newContext();
-  //     context.validate(value);
-  //     expect(context.validationErrors()).toHaveLength(0);
-  //     expect(context.isValid()).toBeTruthy();
-  //   });
-  // });
+  it('tests field as valid', () => {
+    allValidFixtures.forEach(value => {
+      const context = PlaceInfoSchema.newContext();
+      context.validate(value);
+      expect(context.validationErrors()).toHaveLength(0);
+      expect(context.isValid()).toBeTruthy();
+    });
+  });
 });
