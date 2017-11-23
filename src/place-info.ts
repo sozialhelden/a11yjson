76693f15a583e73f6e32b0200fa9bca0451f7bb1
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-23 16:49:58 +0100

    Tested documentation

--- src/place-info.ts
+++ src/place-info.ts
@@ -2,12 +2,31 @@ import SimpleSchema from 'simpl-schema';
 import { Geometry, GeometrySchema } from './geometry';
 import { PlaceProperties, PlacePropertiesSchema } from './place-properties';
 
+/**
+ * The PlaceInfo interface describes a physical location with added accessibility properties.
+ *
+ * It is one of the main objects of accessibility cloud.
+ * A place can have Disruptions and Equipment that can be referenced within the place objects.
+ */
 export interface PlaceInfo {
+  /**
+   * The format version this place info document was created with.
+   * Not used right now, but added for future compatibility.
+   */
   formatVersion?: string;
+  /**
+   * Holds additional place properties such as name and category and accessibility information
+   */
   properties: PlaceProperties;
+  /**
+   * The physical location of the place. Currently only PointGeometry is supported.
+   */
   geometry: Geometry;
 }
 
+/**
+ * The PlaceInfoSchema allows easy validation, cleaning and checking of PlaceInfo objects.
+ */
 export const PlaceInfoSchema = new SimpleSchema({
   formatVersion: {
     type: String,
