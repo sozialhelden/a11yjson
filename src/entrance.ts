3da7347063226bf88d75389616e069c640c27fd4
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-23 15:43:01 +0100

    Removed deleted type

--- src/entrance.ts
+++ src/entrance.ts
@@ -1,5 +1,4 @@
 import SimpleSchema from 'simpl-schema';
-import { EquipmentWithDistanceFromEntrance } from './equipment';
 
 export interface Entrance {
   // QUESTION add entry number
@@ -17,7 +16,7 @@ export interface Entrance {
   // QUESTION could be part of the intercom object, duplication of information
   hasIntercom?: boolean;
   // QUESTION format does not fit with current AC (or maybe my version of the data)
-  intercom?: EquipmentWithDistanceFromEntrance;
+  intercom?: any; // TODO add object type
   payment?: any; // TODO add object type
   // QUESTION duplicated from area
   sitemap?: any; // TODO add object type
