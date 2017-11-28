ef1bf3d0dd153baddd1d08e4a07b177f226fa3e0
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-28 15:30:37 +0100

    Added generation of pot file to build process

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,15 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import camelCase from 'lodash.camelcase';
 
 const pkg = require('./package.json');
 
 const libraryName = 'ac-format';
+
+process.env['NODE_ENV'] = 'production';
+process.env['BABEL_ENV'] = 'production';
 
 export default {
   input: `compiled/${libraryName}.js`,
@@ -24,6 +28,19 @@ export default {
     include: 'compiled/**',
   },
   plugins: [
+    babel({
+      plugins: [
+        [
+          'c-3po',
+          {
+            'extract': {
+              'output': 'dist/ac-format.pot',
+            },
+          },
+        ],
+      ],
+    }),
+
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
 
@@ -31,6 +48,7 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+
 
     // Resolve source maps to the original source
     sourceMaps(),
