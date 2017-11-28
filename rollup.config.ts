0129433a69c8f205501da3e9fb4821be5fb2008c
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-11-28 11:45:03 +0100

    Do not include externals in this module

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,28 +1,32 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import camelCase from 'lodash.camelcase';
 
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-const libraryName = 'ac-format'
+const libraryName = 'ac-format';
 
 export default {
   input: `compiled/${libraryName}.js`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd' },
-    { file: pkg.module, format: 'es' },
+    {file: pkg.main, name: camelCase(libraryName), format: 'umd'},
+    {file: pkg.module, format: 'es'},
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: [
+    'simpl-schema',
+    'c-3po',
+    'js-quantities',
+  ],
   watch: {
     include: 'compiled/**',
   },
   plugins: [
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    
+
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
@@ -31,4 +35,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
