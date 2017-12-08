ac66ff9c987c73a8f916bb60a23a97b5e0cadb09
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2017-12-08 10:30:18 +0100

    Re-Export all the constants

--- src/ac-format.ts
+++ src/ac-format.ts
@@ -5,13 +5,17 @@ import './simpl-schema-extensions';
 export * from './simpl-schema-extensions';
 
 export * from './accessibility';
+export * from './address';
 export * from './area-types';
+export * from './country-codes';
+export * from './door';
 export * from './entrance';
 export * from './equipment-info';
 export * from './equipment-properties';
+export * from './external-id';
 export * from './geometry';
 export * from './personal-profile';
-export * from './external-id';
 export * from './place-info';
 export * from './place-properties';
+export * from './schema-helpers';
 export * from './units';
