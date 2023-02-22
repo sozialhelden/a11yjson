831425d6cb30a4c507102f3e54d28805630930ad
Sebastian Felix Zappe <sebastian@felixzappe.com>
2023-02-22 12:31:53 +0100

    Add Seat#removeArmrests

--- src/Seat.ts
+++ src/Seat.ts
@@ -23,6 +23,7 @@ export const SeatInteractions = [
   'adjustSeatingSurface',
   'adjustSeatingAngle',
   'adjustArmRests',
+  'removeArmrests',
   'adjustHeadRest',
   'adjustLegRest',
   'adjustBackRest',
