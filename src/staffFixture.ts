5838423857e69866643b9571872c51dbbf8b683d
Sebastian Felix Zappe <sebastian@felixzappe.com>
2023-06-27 14:44:01 +0200

    Fix a type error in Staff test

--- src/staffFixture.ts
+++ src/staffFixture.ts
@@ -1,9 +1,10 @@
-import { Staff } from './Staff.js';
 import { Complete } from './Complete.js';
+import { Staff } from './Staff.js';
 
 const staffFixture: Complete<Staff> = {
   canSeeVisitorsFromInside: true,
   hasFreeAssistantForVisitors: true,
-  spokenLanguages: ['es', 'en', 'sgn-ase'],
+  spokenLanguages: ['es', 'en'], // deprecated
+  languages: ['es', 'en', 'sgn-ase'],
 };
 export default staffFixture;
